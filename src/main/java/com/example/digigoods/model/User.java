8c23f378cc25fa20ecd1653314ac93288202b16e
DANS-W202400E0\irfan.jesia <irfan.jesia@dansmultipro.com>
2025-08-29 01:26:18 +0700

    update user object

--- src/main/java/com/example/digigoods/model/User.java
+++ src/main/java/com/example/digigoods/model/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,4 +30,35 @@ public class User {
 
   @Column(nullable = false)
   private String password;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "phone_number")
+  private String phoneNumber;
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  /**
+   * Constructor for basic user creation with username and password.
+   *
+   * @param username the username
+   * @param password the password
+   */
+  public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+    this.createdAt = LocalDateTime.now();
+    this.updatedAt = LocalDateTime.now();
+  }
 }
